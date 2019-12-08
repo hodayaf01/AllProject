3a1fcbd381b1b1cf151f1a2575951ad1c88dcf0d
shiralulvi1@gmail.com <shiralulvi1@gmail.com>
2019-12-08 23:40:29 +0200

    12.8.19 עכשיו תעבדי על כל השגיאות

--- c#/BL/TimeOfDay_BL.cs
+++ c#/BL/TimeOfDay_BL.cs
@@ -1,10 +1,10 @@
-﻿using DAL;
-using Models;
+﻿using Models;
 using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using DAL;
 
 namespace BL
 {
