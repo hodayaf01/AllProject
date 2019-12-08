a6f9596d732490273951d25b84ad73350760e3f5
hodayaf <HFarkash@amdocs.com>
2019-12-09 00:02:33 +0200

    remove marks

--- example/src/app/Models/Guardian.model.ts
+++ example/src/app/Models/Guardian.model.ts
@@ -4,7 +4,7 @@ export class Guardian{
     PhoneNumber:string
 
     constructor(Id?:number,guardianName?:string,PhoneNumber?:string){
-        this.Id=Id;//check if I need to send the ID
+        this.Id=Id;
         this.guardianName=guardianName;
         this.PhoneNumber=PhoneNumber;
     }
