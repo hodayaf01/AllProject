7c61fda59c93fd9bb9e439bd73070ccd6f951d89
hodaya.farkash@gmail.com <hodaya.farkash@gmail.com>
2020-05-10 17:20:32 +0300

    adding portal page + css changes

---
+++ example/src/app/Components/Portal/Portal.ts
@@ -0,0 +1,18 @@
+
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+    selector: 'app-Portal',
+    templateUrl: './Portal.html',
+    styleUrls: ['./Portal.css']
+})
+
+export class PortalComponent implements OnInit{
+    
+    
+    
+    ngOnInit(): void {
+        throw new Error("Method not implemented.");
+    }
+
+}
